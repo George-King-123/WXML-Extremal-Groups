352c258fca68a59d13eec9dab89cbe8476a77ede
George King <emailgeorgeatthisaddress@gmail.com>
2025-12-13 15:48:05 +0100

    some zd changes

--- Z_d_product_Z_d.py
+++ Z_d_product_Z_d.py
@@ -1,3 +1,5 @@
+# run with Python -m Z_d_product_Z_d
+
 from shared_code import get_sparse_d_tuple, compute_Sn, incl_range
 from group_operations import op_Z_d_Z_d
 from math import comb, ceil, floor, factorial
@@ -25,13 +27,6 @@ def compute_s_n_with_sign_distribution(n, d, distribution):
   s_n = compute_Sn(S, n, op_Z_d_Z_d)
   return len(s_n)
 
-def show_we_only_need_one(n, k, d):
-  pass
-
-# formula for |S^n| when |S| = k, and there are t elements
-# with a 1 in the 2nd component, k-t elements with a 0
-# (i.e. no other signs)
-# We can prove this formula and it matches with all simulations
 def formula_when_all_zero_one_in_snd_comp(n, k, p, d):
   if p == 0:
     return comb(n + k -1, k - 1)
@@ -50,74 +45,64 @@ def formula_when_all_zero_one_in_snd_comp(n, k, p, d):
     res += cur_term
   return res
 
-# We think the largest value of |S^n| is always achieved with just 0 and 1 
-# in the second component (we DONT have a proof of this). 
-# Thus we only need to check how many 1s to use, 
-# i.e. try all possibly values of t from 0 to k in the above formula
-def best_with_ones(k, n, d, noisy = False):
-  best = -1
-  achieved_at = -1
-  for t in range(0, k+1):
-    val = formula_when_all_zero_one_in_snd_comp(n, k, t, d)
-    if val > best:
-      best = val
-      achieved_at = t 
+
+# test that the formula for just 0s and 1s in the second component
+# works for all values of d,n,k up to the given upper bounds
+# these have to be small because we explicitly compute |S^n| in order 
+# to check the formula 
+# ex: test_formula_all_one_many_vals(5, 5, 5)
+def test_formula_all_one_many_vals(d_upper, n_upper, k_upper):
+  print("testing that the Z_d formula works")
+  def formula_works(n, k, p, d):
+    S = make_S(d = d, k = k, distro_of_signs= [k - p, p] + ([0] * (d-2)))
+    s_n = compute_Sn(S, n, op_Z_d_Z_d)
+    return len(s_n) == formula_when_all_zero_one_in_snd_comp(n, k, p, d)
   
-  if noisy:
-    print("k = {}, n = {}, best at {} signs".format(k, n, achieved_at))
+
+  for d in incl_range(2, d_upper):
+    for n in incl_range (1, n_upper):
+      for k in incl_range(1, k_upper):
+        for p in incl_range(0, k):
+          if not formula_works(n, k, p, d):
+            print(f"FAIL {n=} {k=} {p=} {d=}")
+            return
+          
+  print("success")
+
+
+# test \lim_{n \to \infty} formula(t, d, k, n) / n^{d(k-1)}. 
+# This didn't end up being important in the paper
+# This call doesn't take too long: test_predicted_limit(big_number= 10 ** 6, k=10, p=4, d=7)
+def test_predicted_limit(big_number, k, p, d):
+    def analytic_limit(k, p, d):
+      numerator = factorial(d * (p - 1))
+      denominator = ((factorial(p - 1)) ** d) * (d ** (d * (p - 1))) * factorial(d * (k - 1))
+      return numerator / denominator
+
+    empirical_limit = formula_when_all_zero_one_in_snd_comp(n=big_number, k=k, p=p, d=d) / big_number ** (d * (k - 1))
+    print(f"{empirical_limit=}")
     
-  return best
-
-
-# Conjecture: when k is fixed, n gets large we get an exponent of n^{d(k-1)}
-# with a coefficient of 1/(d(k-1)!)
-# I think we had a start of a proof with this. It matches with the simulation below,
-# n has to be quite large for it to converge. 
-# e.x. try exponent(1000, 5, 3) and exponent(10000, 5, 3)
-def exponent(n, k, d):
-  print("\'Actual\'")
-  size_s_n = best_with_ones(n=n, k=k, d=d)
-  print(size_s_n/(n**(d * (k - 1))))
-  
-  print("Conjecture:")
-  print(1/(factorial(d*(k - 1))))
-
-
-
-# we are interested in \lim_{n \to \infty} formula(t, d, k, n) / n^{d(k-1)} 
-def predicted_limit(k, t, d):
-    numerator = factorial(d * (t - 1))
-    denominator = ((factorial(t - 1)) ** d) * (d ** (d * (t - 1))) * factorial(d * (k - 1))
-    return numerator / denominator
-
-def test_predicted_limit(big_number, k, t, d):
-    numerical_approx_of_limit = formula_when_all_zero_one_in_snd_comp(n=big_number, k=k, p=t, d=d) / big_number ** (d * (k - 1))
-    print(f"Numerical approximation of limit: {numerical_approx_of_limit}")
-    predicted = predicted_limit(k, t, d)
-    print(f"Predicted limit: {predicted}")
-
-    print(f"Difference: {abs(numerical_approx_of_limit - predicted)}")
-
-def find_best_t_val_limit(k, d):
-    return max(range(1, k+1), key=lambda t: predicted_limit(k, t, d))
-
-def main():
-  # test_predicted_limit(big_number= 10 ** 6, k=10, t=4, d=7)
-  print(find_best_t_val_limit(k=20, d=2))
+    analytic_limit_value = analytic_limit(k, p, d)
+    print(f"{analytic_limit_value=}")
 
 def find_maximizing_t_val(n, k, d): 
     maximizers = []
     maximimum = -1 
 
-    for t in incl_range(0, k): 
-        cur_val = formula_when_all_zero_one_in_snd_comp(n=n, k=k, p=t, d=d)
+    for p in incl_range(0, k): 
+        cur_val = formula_when_all_zero_one_in_snd_comp(n=n, k=k, p=p, d=d)
         if cur_val > maximimum:
-            maximizers = [t]
+            maximizers = [p]
             maximimum = cur_val 
         elif cur_val == maximimum:
-            maximizers.append(t) 
+            maximizers.append(p) 
     
     return maximizers
+
+def main():
+  # test_formula_all_one_many_vals(6, 6, 5)
+  print("Not doing anything, look at main() for options of what this script can do")
+
 
 if __name__ == "__main__":
   main()
